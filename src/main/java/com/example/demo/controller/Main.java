4d1784caed24581cbb81d2e5f7440d67c31bae10
levyy15 <sophieaaurora@gmail.com>
2024-11-26 15:18:37 +0800

    testing1

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -4,7 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+//testingtesting
 public class Main extends Application {
 
 	private static final int SCREEN_WIDTH = 1300;
